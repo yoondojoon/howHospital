5f7ff109396d3f079b88e2e3651315bf9587301a
haeunsh <95haeunsh@gmail.com>
2024-02-16 18:12:29 +0900

    240216 공통작업 시작

---
+++ src/main/java/kr/or/iei/HomeController.java
@@ -0,0 +1,19 @@
+package kr.or.iei;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value="/")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping(value="/ref")
+	public String ref() {
+		return "ref";
+	}
+	
+}
