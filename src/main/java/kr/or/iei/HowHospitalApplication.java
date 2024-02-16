3135632fd1520baabae07a06f85b6779807a301d
haeunsh <95haeunsh@gmail.com>
2024-02-16 15:56:13 +0900

    Initial commit

---
+++ src/main/java/kr/or/iei/HowHospitalApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/or/iei/HowHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
