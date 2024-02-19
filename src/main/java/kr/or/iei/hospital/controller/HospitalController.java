4733f9c220976d10866b6c8d3908722c01302d76
luciankim <devlucian7@gmail.com>
2024-02-19 17:34:18 +0900

    초기 경로 세팅

--- src/main/java/kr/or/iei/hospital/controller/HospitalController.java
+++ src/main/java/kr/or/iei/hospital/controller/HospitalController.java
@@ -1,10 +1,16 @@
 package kr.or.iei.hospital.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/Hospital")
+@RequestMapping(value="/hospital")
 public class HospitalController {
-
+	
+	@GetMapping(value="/businessAuth")
+	public String businessAuth() {
+		return "hospital/businessAuth";
+	}
+	
 }
