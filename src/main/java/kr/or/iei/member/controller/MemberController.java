62b4da0ce62cd304001e24d4e62119c77bc18b36
L1E1e <99781037+L1E1e@users.noreply.github.com>
2024-02-19 17:45:05 +0900

    v2 커밋

--- src/main/java/kr/or/iei/member/controller/MemberController.java
+++ src/main/java/kr/or/iei/member/controller/MemberController.java
@@ -1,10 +1,21 @@
 package kr.or.iei.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value="/member")
 public class MemberController {
 
+	
+	
+	@PostMapping(value="/signUpFrm")
+	public String signUpFrm() {
+		
+		
+		return "member/signUpFrm";
+		
+	}
+	
 }
