be786646a5a087ec127c333a14a6af35ce88f1e9
haeunsh <95haeunsh@gmail.com>
2024-02-19 16:36:04 +0900

    컨트롤러 생성

---
+++ src/main/java/kr/or/iei/hospital/controller/HospitalController.java
@@ -0,0 +1,10 @@
+package kr.or.iei.hospital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/Hospital")
+public class HospitalController {
+
+}
