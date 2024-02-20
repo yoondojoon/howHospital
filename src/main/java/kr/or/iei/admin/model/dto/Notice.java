6f53c884bcb503233c330259233630612d969da6
ascarlon7777 <159400522+ascarlon7777@users.noreply.github.com>
2024-02-20 11:05:28 +0900

    수정

---
+++ src/main/java/kr/or/iei/admin/model/dto/Notice.java
@@ -0,0 +1,17 @@
+package kr.or.iei.admin.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Notice {
+	private int noticeNo;
+	private int memberNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private int readCount;
+	private String ReqDate;
+}
