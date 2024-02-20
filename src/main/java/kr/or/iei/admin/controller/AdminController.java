461f6ad6dd076544060f06865db9afb767a971e3
ascarlon7777 <159400522+ascarlon7777@users.noreply.github.com>
2024-02-20 10:16:59 +0900

    수정

--- src/main/java/kr/or/iei/admin/controller/AdminController.java
+++ src/main/java/kr/or/iei/admin/controller/AdminController.java
@@ -1,10 +1,15 @@
 package kr.or.iei.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value="/admin")
 public class AdminController {
 	
+	@GetMapping(value="/adminMain")
+	public String adminMain() {
+		return "/admin/adminMain";
+	}
 }
