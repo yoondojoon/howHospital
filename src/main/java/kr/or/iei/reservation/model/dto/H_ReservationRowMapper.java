0af0f6855a0fe5b505a5ba612b96db6e8afc9764
yoondojoon <blueyerr12@naver.com>
2024-02-23 18:27:49 +0900

    병원예약상세보기 커밋

--- src/main/java/kr/or/iei/reservation/model/dto/H_ReservationRowMapper.java
+++ src/main/java/kr/or/iei/reservation/model/dto/H_ReservationRowMapper.java
@@ -19,6 +19,7 @@ public class H_ReservationRowMapper implements RowMapper<H_Reservation>{
 		hr.setReservationType(rs.getInt("reservation_type"));
 		hr.setReservationNo(rs.getInt("reservation_no"));
 		hr.setPrescriptionStatus(rs.getInt("prescription_status"));
+		hr.setReservationTime(rs.getString("reservation_time"));
 		return hr;
 	}
 	
--- src/main/java/kr/or/iei/reservation/model/service/ReservationDetailService.java
+++ src/main/java/kr/or/iei/reservation/model/service/ReservationDetailService.java
@@ -1,9 +1,12 @@
 package kr.or.iei.reservation.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.iei.reservation.model.dao.ReservationDetailDao;
+import kr.or.iei.reservation.model.dto.H_Reservation;
 import kr.or.iei.reservation.model.dto.ReservationDetail;
 
 @Service
@@ -14,5 +17,10 @@ public class ReservationDetailService {
 	public String selectDoctor() {
 		String doctorName = reservationDetailDao.selectDoctor();
 		return doctorName;
+	}
+
+	public ReservationDetail selectOneReservation(H_Reservation hr) {
+		ReservationDetail rd = reservationDetailDao.selectOneReservation(hr);
+		return rd;
 	}
 }
