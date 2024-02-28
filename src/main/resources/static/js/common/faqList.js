81a38426dbd1f6667bdef17d23bf3c6c966d7b52
ascarlon7777 <159400522+ascarlon7777@users.noreply.github.com>
2024-02-28 11:36:26 +0900

    FAQ 본문 수정

--- src/main/resources/static/js/common/faqList.js
+++ src/main/resources/static/js/common/faqList.js
@@ -1,36 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
- 	const accordionItems = document.querySelectorAll('.accordion-item');
+    const accordionItems = document.querySelectorAll('.accordion-item');
 
-	accordionItems.forEach(item => {
-	 const header = item.querySelector('.accordion-header');
-	 const toggleBtn = header.querySelector('.toggle-btn');
-	 const content = item.querySelector('.accordion-content');
-	 const subAccordion = item.querySelector('.accordion-sub');
+    accordionItems.forEach(item => {
+        const header = item.querySelector('.accordion-header');
+        const toggleBtn = header.querySelector('.toggle-btn');
+        const contentAll = item.querySelectorAll('.accordion-content');
+        const subAccordionItems = item.querySelectorAll('.accordion-sub');
 
-	// 1. 처음에는 subAccordion이 hide 상태
-	 if (subAccordion) {
-	 	subAccordion.style.display = 'none';
-	 	}
+        // 1. 처음에는 subAccordion이 hide 상태
+        if (subAccordionItems) {
+            subAccordionItems.forEach(subAccordionItem => {
+                subAccordionItem.style.display = 'none';
+            });
+        }
 
-	toggleBtn.addEventListener('click', function() {
-		 item.classList.toggle('active');
-		 // 2. header의 toggleBtn 클릭 시 subAccordion이 펼쳐짐
-		 if (subAccordion) {
-		 subAccordion.style.display = item.classList.contains('active') ? 'block' : 'none';
-		
-		// 2-1. subAccordion에서 content는 hide 상태
-		 content.style.display = 'none';
-	 	}
-	
-	// 2-2. subAccordion의 toggleBtn 클릭 시 content가 토글
-		 if (subAccordion) {
-		 const subToggleBtn = subAccordion.querySelector('.toggle-btn');
-		 subToggleBtn.addEventListener('click', function() {
-			 content.style.display = content.style.display === 'none' ? 'block' : 'none';
-		 	});
-		 }
-		
-		toggleBtn.textContent = item.classList.contains('active') ? 'expand_less' : 'expand_more';
-		 });
-	 });
+        toggleBtn.addEventListener('click', function() {
+            item.classList.toggle('active');
+            // 2. header의 toggleBtn 클릭 시 subAccordion이 펼쳐짐
+            if (subAccordionItems) {
+                subAccordionItems.forEach(subAccordionItem => {
+                    subAccordionItem.style.display = item.classList.contains('active') ? 'block' : 'none';
+                });
+
+            }
+
+           subAccordionItems.forEach(subAccordionItem => {
+			    const subAccordion = subAccordionItem.querySelector('.accordion-sub');
+			    const content = subAccordionItem.querySelector('.accordion-content');
+			    const subToggleBtn = subAccordionItem.querySelector('.toggle-btn');
+				
+				
+			    // 2-2. subAccordion의 toggleBtn 클릭 시 content가 토글
+			    if (!subAccordion) {
+			        subToggleBtn.addEventListener('click', function() {
+			            content.style.display = content.style.display === 'none' ? 'block' : 'none';
+			            subToggleBtn.textContent = content.style.display === 'none' ? 'expand_more' : 'expand_less';
+			        });
+			    }
+			});
+
+            toggleBtn.textContent = item.classList.contains('active') ? 'expand_less' : 'expand_more';
+        });
+    });
 });
